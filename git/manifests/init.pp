d94ca939196489d4e19c69f2bf98d31324199683
Juuso Puroila <jpuroila@gmail.com>
2017-04-12 00:22:08 +0300

    Add .gitconfig

--- git/manifests/init.pp
+++ git/manifests/init.pp
@@ -2,6 +2,9 @@ class git{
 	file {'/etc/bash.bashrc':
 		content =>template('git/bash.bashrc.erb')
 	}
+	file{'/etc/.gitconfig':
+		content =>template('git/bash.bashrc.erb')
+	}
 	package {git:
 		ensure => latest,
 		allowcdrom => true
