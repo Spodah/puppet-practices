f2f8876a53695227c47fcef6a2e4ee6a91adb351
Juuso Puroila <jpuroila@gmail.com>
2017-04-12 13:22:00 +0000

    Add apache2.conf

--- apache/manifests/init.pp
+++ apache/manifests/init.pp
@@ -4,9 +4,20 @@ class apache {
 		allowcdrom =>true
 	}
 	file{'/etc/apache2/sites-available/000-default.conf':
-		content =>template('apache/000-default.conf.erb')
+		content =>template('apache/000-default.conf.erb'),
+		notify =>Service['apache2']
 	}
 	file{'/etc/apache2/sites-enabled/000-default.conf':
-		content =>template('apache/se-000-default.conf.erb')
+		content =>template('apache/se-000-default.conf.erb'),
+		notify =>Service['apache2']
+	}
+	file{'/etc/apache2/apache2.conf':
+		content =>template('apache/apache2.conf.erb'),
+		notify =>Service['apache2']
+	}
+	service{'apache2':
+		ensure =>true,
+		enable =>true,
+		provider =>systemd
 	}
 }
