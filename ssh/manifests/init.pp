2fe7ab4af867604e70be89a71655b9c612ffb6ce
Juuso Puroila <jpuroila@gmail.com>
2017-04-12 12:12:55 +0000

    add sshd_config template, change port to 2222, allow login without password

--- ssh/manifests/init.pp
+++ ssh/manifests/init.pp
@@ -3,4 +3,14 @@ class ssh {
 		ensure =>installed,
 		allowcdrom => true
 	}
+	
+	file{'/etc/ssh/sshd_config':
+		content =>template('ssh/sshd_config.erb'),
+		notify =>Service['ssh']
+	}
+	service{'ssh':
+		ensure =>true,
+		enable =>true,
+		provider =>systemd
+	}
 }
