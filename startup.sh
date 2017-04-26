165ad1320e9008df5bca0a32896b2d2c47571807
Juuso Puroila <Spodah@users.noreply.github.com>
2017-04-26 15:42:37 +0300

    update startup.sh with apt-get update

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 setxkbmap fi
+sudo apt-get update
 sudo apt-get install -y git puppet
 git clone https://github.com/Spodah/puppet-practices.git
 sudo puppet apply --modulepath /home/kubuntu/puppet-practices -e class{"git:"}
