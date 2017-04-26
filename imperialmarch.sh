015e4d61f18488aa7c471a4b5dc7859107f87378
Juuso Puroila <jpuroila@gmail.com>
2017-04-26 23:33:44 +0300

    Slow imperialmarch.sh

--- imperialmarch.sh
+++ imperialmarch.sh
@@ -1,7 +1,7 @@
-beep -f207.65 -l500 -r3 -D10
-beep -f155.56 -l375 -D10
-beep -f246.94 -l125 -D10
-beep -f207.65 -l500 -D10
-beep -f155.56 -l375 -D10
-beep -f246.94 -l125 -D10
-beep -f207.65 -l2000 -D10
+beep -f207.65 -l600 -r3 -D10
+beep -f155.56 -l450 -D10
+beep -f246.94 -l150 -D10
+beep -f207.65 -l600 -D10
+beep -f155.56 -l450 -D10
+beep -f246.94 -l150 -D10
+beep -f207.65 -l2400 -D10
