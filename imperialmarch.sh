22139bd87a6be27e09862f307c92b087bbaa8a95
Juuso Puroila <jpuroila@gmail.com>
2017-04-26 23:43:02 +0300

    correct imperialmarch

--- imperialmarch.sh
+++ imperialmarch.sh
@@ -1,7 +1,7 @@
-beep -f207.65 -l600 -r3 -D10
-beep -f155.56 -l450 -D10
-beep -f246.94 -l150 -D10
-beep -f207.65 -l600 -D10
-beep -f155.56 -l450 -D10
-beep -f246.94 -l150 -D10
-beep -f207.65 -l2400 -D10
+beep -f207.65 -l700 -r3 -D20
+beep -f155.56 -l525 -D20
+beep -f246.94 -l125 -D20
+beep -f207.65 -l700 -D20
+beep -f155.56 -l525 -D20
+beep -f246.94 -l175 -D20
+beep -f207.65 -l1400 -D20
