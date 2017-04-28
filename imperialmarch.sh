cd18884a9615753c26ee1d12aad455ec907fe97b
Juuso Puroila <jpuroila@gmail.com>
2017-04-28 12:18:56 +0300

    update imperialmarch

--- imperialmarch.sh
+++ imperialmarch.sh
@@ -1,7 +1,14 @@
 beep -f207.65 -l700 -r3 -D20
 beep -f155.56 -l525 -D20
-beep -f246.94 -l125 -D20
+beep -f233.08 -l125 -D20
 beep -f207.65 -l700 -D20
 beep -f155.56 -l525 -D20
-beep -f246.94 -l175 -D20
+beep -f233.08 -l175 -D20
 beep -f207.65 -l1400 -D20
+beep -f293.66 -l700 -r3  -D20
+beep -f311.13 -l525 -D20
+beep -f233.08 -l175 -D20
+beep -f185 -l700 -D20
+beep -f164.81 -l525 -D20
+beep -f233.08 -l175 -D20
+beep -f196 -l1400 -D20
